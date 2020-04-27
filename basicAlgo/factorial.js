4e9ec382c5c7677ea845430f9ecd4a85b4f50c56
emmaodia <odia.emma@gmail.com>
2020-04-27 01:45:05 +0100

    Solved factorial algo.

---
+++ basicAlgo/factorial.js
@@ -0,0 +1,12 @@
+function factorialize(num) {
+    let arr = [];
+    let total = 1;
+  
+    for(var i = 0; i < num; i++){
+      console.log(total *= arr.push(num[i]));
+    }
+    return total;
+  }
+  
+  factorialize(5);
+  
